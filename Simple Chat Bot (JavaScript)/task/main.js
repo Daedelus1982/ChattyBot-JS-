f1a8d031659fb700fc8fe1bdc4b7a06d6cc28957
James Hawthorne <Daedelus1982@users.noreply.github.com>
2023-11-26 14:34:43 +0000

    stage 5

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,2 +1,67 @@
-console.log('Hello! My name is Dave.');
-console.log('I was created in 2023.');
+const input = require('sync-input');
+
+
+function greet(bot_name, birth_year) {
+	console.log("Hello! My name is " + bot_name + ".");
+	console.log("I was created in " + birth_year + ".");
+}
+
+function remind_name() {
+	console.log("Please, remind me your name.");
+	let name = input();
+	console.log("What a great name you have, " + name + "!");
+}
+
+function guess_age() {
+	console.log("Let me guess your age.");
+	console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+	let rem3 = Number(input());
+	let rem5 = Number(input());
+	let rem7 = Number(input());
+
+	let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+	console.log("Your age is " + age + "; that's a good time to start programming!");
+}
+
+function count() {
+	console.log("Now I will prove to you that I can count to any number you want.");
+
+	let number = Number(input());
+	let current = 0;
+
+	while (current <= number) {
+		console.log(current + " !");
+		current += 1;
+	}
+}
+
+function test() {
+	console.log("Let's test your programming knowledge.");
+
+    console.log("What does CI mean?");
+    console.log("1. Continuous Input");
+    console.log("2. Convenient Integration");
+    console.log("3. Continuous Integration");
+    console.log("4. Conflabs Internals");
+
+    let answer = Number(input());
+
+    while (answer != 3) {
+        console.log("Please, try again.")
+        answer = Number(input());
+    }
+}
+
+function end() {
+	console.log("Congratulations, have a nice day!");
+}
+
+
+greet('Aid', '2020')  // change it as you need
+remind_name();
+guess_age();
+count();
+test();
+end();
